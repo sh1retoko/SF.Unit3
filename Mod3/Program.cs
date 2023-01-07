8e84071190622939b8b8dcc8d2ef7dd5fd26b8ff
sh1retoko <120920943+sh1retoko@users.noreply.github.com>
2023-01-07 18:29:26 +0300

    Add files via upload

---
+++ Mod3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mod3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            byte age = ((byte)int.Parse(Console.ReadLine()));
+
+            Console.WriteLine($"\nYour name is {name} and age {age}");
+
+            Console.Write("\nEnter your birthdate date: ");
+            var day = Console.ReadLine();
+
+            Console.WriteLine($"Your britdate is {day}");
+
+            Console.ReadKey();
+        }
+    }
+}
+
